dcc36eede949af11a56e63d4c76d9e255f99bc87
Victor Franco <vfrancohdez1@hotmail.com>
2024-01-27 13:59:14 -0600

    feat: add Mobile component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './components/Header.tsx'
+import Mobile from './components/Mobile.tsx'
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
-    </>
+      <main>
+        <Mobile />
+      </main>
+    </div>
   )
 }
 
---
+++ src/components/Mobile.tsx
@@ -0,0 +1,9 @@
+import './Mobile.css'
+
+function Mobile() {
+  return (
+    <div className="Mobile"></div>
+  )
+}
+
+export default Mobile
